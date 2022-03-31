5182b61d95c0cf06f44ceb45840794f4c87ad8b8
François Lespinasse <francois.lespinasse96@gmail.com>
2022-03-31 04:56:27 -0400

    added make features

--- flesp_friends-isc/src/features/build_features.py
+++ flesp_friends-isc/src/features/build_features.py
@@ -1,39 +1,50 @@
+import os
+import click
+import logging
+from dotenv import find_dotenv, load_dotenv
+import glob
 import numpy as np
-
 from brainiak.isc import isc
 from brainiak import io, image
 import nibabel as nib
+from nilearn.datasets import load_mni152_template
 
-project_dir = Path(__file__).resolve().parents[2]
 subjects = ['sub-01', 'sub-02', 'sub-03',
             'sub-04', 'sub-05', 'sub-06']
 
 # make sure it's installed
-mask_name = 'tpl-MNI152NLin2009cAsym_res-02_desc-brain_mask.nii.gz'
-brain_mask = io.load_boolean_mask(mask_name)
+brain_mask = load_mni152_template()
 coords = np.where(brain_mask)
-brain_nii = nib.load(mask_name)
+brain_nii = nib.load(brain_mask)
 
 
-def compute_bold_isc(episodes_path, pairwise=pairwise):
+@click.command()
+@click.argument('postproc_path', type=click.Path(exists=True))
+@click.argument('isc_map_path', type=click.Path(exists=True))
+def map_isc(postproc_path, isc_map_path, pairwise=False):
     """
     Compute ISC for brain data.
 
     note
     """
     # specify data path (leads to subdirs)
-    MultiSubject_episode_path = f'{project_dir}{episodes_path}'
+    logger = logging.getLogger(__name__)
+    logger.info('Starting Temporal ISC workflow')
     # walks subdirs with taks name (task-s01-e01a)
-    for task in glob.glob(MultiSubject_episode_path):
-        files = sorted(glob.glob(f'{task}*fwhm*_bold.*'))
+    for task in glob.glob(f"{postproc_path}/*/"):
+        task = os.path.basename(task)
+        logger.info(f'Importing data')
+        files = sorted(glob.glob(f'{postproc_path}/{task}/*.nii.gz*'))
 
         images = io.load_images(files)
         masked_imgs = image.mask_images(images, brain_mask)
 
         # compute ISC
         bold_imgs = image.MaskedMultiSubjectData.from_masked_images(
-            masked_imgs, 6)
+                masked_imgs, 6)
+        # replace nans
         bold_imgs[np.isnan(bold_imgs)] = 0
+        # compute ISC
         isc_imgs = isc(bold_imgs, pairwise=pairwise)
 
         # save ISC maps per subject
@@ -48,8 +59,27 @@ def compute_bold_isc(episodes_path, pairwise=pairwise):
             )
 
             # Save the ISC data as a volume
-            isc_map_path = f'ISC_{task}_{subj}.nii.gz'
-            nib.save(isc_nifti, isc_map_path)
+            try:
+                nib.save(isc_nifti, f'{isc_map_path}/{task}/temporalISC_{task}'
+                                    f'_{subj}.nii.gz')
+            except FileNotFoundError:
+                os.path.mkdir(f"{isc_map_path}/{task}")
+                nib.save(isc_nifti, f'{isc_map_path}/{task}/temporalISC_{task}'
+                                    f'_{subj}.nii.gz')
 
         # free up memory
         del bold_imgs, isc_imgs
+
+
+if __name__ == '__main__':
+    # NOTE: from command line `make_dataset input_data output_filepath`
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+    map_isc()
