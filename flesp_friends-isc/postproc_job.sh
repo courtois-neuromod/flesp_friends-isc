ff8a9311dd2f18be2b7b9f5fdfe4bb6ad5c4d08b
François Lespinasse <francois.lespinasse96@gmail.com>
2022-03-30 21:09:25 -0400

    job file

--- flesp_friends-isc/postproc_job.sh
+++ flesp_friends-isc/postproc_job.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=postproc
 #SBATCH --output=./output_file.out
 #SBATCH --error=./error_file.err
-#SBATCH --time=6:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=16G
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=24G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s5c7o8a6s1u8i7u8@courtois-neuromod.slack.com
 
