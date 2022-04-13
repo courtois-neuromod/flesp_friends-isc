00fe6f392aa17160c6b10c882305bffac1f41f9b
François Lespinasse <francois.lespinasse96@gmail.com>
2022-04-13 09:52:07 -0400

    more time

--- flesp_friends-isc/viz_job.sh
+++ flesp_friends-isc/viz_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=viz-surf
 #SBATCH --output=./viz_3.out
 #SBATCH --error=./viz_3.err
-#SBATCH --time=3:00:00
+#SBATCH --time=5:00:00
 #SBATCH --mem=48G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s5c7o8a6s1u8i7u8@courtois-neuromod.slack.com
