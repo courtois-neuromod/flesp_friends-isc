e60831bc0317993d57256dcb032e65715659327c
François Lespinasse <francois.lespinasse96@gmail.com>
2022-07-14 08:48:36 -0400

    job spec

--- flesp_friends-isc/viz_job.sh
+++ flesp_friends-isc/viz_job.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=viz-surf
 #SBATCH --output=./viz_3.out
 #SBATCH --error=./viz_3.err
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s5c7o8a6s1u8i7u8@courtois-neuromod.slack.com
 
 workon flesp_friends-isc
 cd flesp_friends-isc
-make figures USER_OPTIONS='--slices True --kind temporal'
+make figures USER_OPTIONS='--slices True --kind temporal --pairwise True'
